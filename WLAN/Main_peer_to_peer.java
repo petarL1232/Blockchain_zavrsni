c6a4f149739c2755fed442e6dbabb44b9e7b6c99
Petar <neki.randomalt@gmail.com>
2026-09-06 18:31:42 +0200

    PING PONG

--- WLAN/Main_peer_to_peer.java
+++ WLAN/Main_peer_to_peer.java
@@ -21,68 +21,26 @@ public class Main_peer_to_peer {
 
         // Client client = new Client(Ipconfig.IP_OF_MY_PC, Ipconfig.PORT);
 
-
-
         if (Ipconfig.SERVER_MODE) {
             runServer();
         } else {
             runClient();
         }
 
-
-
     }
 
     private static void runServer() throws Exception {
 
-        try (Server server = new Server(Ipconfig.PORT); PeerConnection connection = server.acceptConnection()) {
+        try (Server server = new Server(Ipconfig.PORT)) {
 
-            NetworkMessage receivedMessage = connection.receive();
+            while (true) {
+                PeerConnection connection = server.acceptConnection();
+                Thread peerThread = new Thread(() -> handleIncomingPeer(connection)); // prvi Thread
+                peerThread.setName("peer-" + connection.getRemoteAddress() + "-" + connection.getRemotePort());
 
-            if (receivedMessage == null) {
-                System.out.println("Peer se odspojio prije HELLO poruke.");
-                return;
+                peerThread.start();
             }
-
-            if (receivedMessage.getType() != MessageType.HELLO) {
-                NetworkMessage rejectMessage = MessageCodec.createMessage(
-                        MessageType.REJECT,
-                        Ipconfig.NODE_ID,
-                        receivedMessage.getMessageId(),
-                        new RejectPayload(
-                                "EXPECTED_HELLO",
-                                "Prva poruka mora biti HELLO."));
-
-                connection.send(rejectMessage);
-                return;
-            }
-
-            HelloPayload peerInfo = MessageCodec.payloadAsPayloadTypeIWant(receivedMessage,HelloPayload.class);
-
-            String rejectionReason = validateHello(
-                    receivedMessage,
-                    peerInfo);
-
-            boolean accepted = rejectionReason == null;
-            HelloPayload ownInfo = null;
-
-            if (accepted) {
-                ownInfo = createHelloPayload();
-                System.out.println("Prihvaćen node: " + receivedMessage.getSenderNodeId());
-                System.out.println("Node type: "+ peerInfo.getNodeType());
-            } else {
-                System.out.println("HELLO odbijen: " + rejectionReason);
-            }
-
-            HelloAckPayload ackPayload = new HelloAckPayload(accepted,rejectionReason,ownInfo);
-
-            NetworkMessage ackMessage = MessageCodec.createMessage(MessageType.HELLO_ACK,Ipconfig.NODE_ID, receivedMessage.getMessageId(), ackPayload);
-
-            connection.send(ackMessage);
-        }   
-        //catch (Exception e) { previše se javlja grešaka mozda ce se u buducnosti napraviti samo jedan catch da se ne mora sve pisat
-            // TODO: handle exception
-        //}
+        }
     }
 
     private static void runClient() throws Exception {
@@ -109,15 +67,16 @@ public class Main_peer_to_peer {
 
             if (response.getType() == MessageType.REJECT) {
 
-                RejectPayload rejectPayload = MessageCodec.payloadAsPayloadTypeIWant(response,RejectPayload.class);
+                RejectPayload rejectPayload = MessageCodec.payloadAsPayloadTypeIWant(response, RejectPayload.class);
 
-                System.out.println("Konekcija odbijena: " + rejectPayload.getCode() + " | " + rejectPayload.getReason());
+                System.out
+                        .println("Konekcija odbijena: " + rejectPayload.getCode() + " | " + rejectPayload.getReason());
 
                 return;
             }
 
             if (response.getType() != MessageType.HELLO_ACK) {
-                System.out.println("Očekivan je HELLO_ACK.");
+                System.out.println("Ocekivan je HELLO_ACK.");
                 return;
             }
 
@@ -153,6 +112,53 @@ public class Main_peer_to_peer {
             System.out.println("Server node: " + response.getSenderNodeId());
 
             System.out.println("Server type: " + serverInfo.getNodeType());
+
+            pingServer(client);
+        }
+    }
+
+    private static void pingServer(Client client) throws Exception {
+
+        while (true) {
+            long sentAt = System.currentTimeMillis();
+            PingPayload pingPayload = new PingPayload(sentAt);
+
+            NetworkMessage pingMessage = MessageCodec.createMessage(
+                    MessageType.PING,
+                    Ipconfig.NODE_ID,
+                    null,
+                    pingPayload);
+
+            client.send(pingMessage);
+
+            NetworkMessage response = client.receive();
+
+            if (response == null) {
+                System.out.println("Server se odspojio.");
+                return;
+            }
+
+            if (response.getType() != MessageType.PONG) {
+                System.out.println("Očekivan je PONG, primljen je: "+ response.getType());
+                return;
+            }
+
+            if (!pingMessage.getMessageId().equals(response.getReplyToId())) {
+                System.out.println("PONG ne odgovara poslanoj PING poruci.");
+
+                return;
+            }
+
+            PongPayload pongPayload = MessageCodec.payloadAsPayloadTypeIWant(response,PongPayload.class);
+
+            if (pongPayload.getPingSentAt() != sentAt) {
+                System.out.println("PONG sadrži pogrešan timestamp.");
+                return;
+            }
+
+            long responseTime = System.currentTimeMillis() - sentAt; // da se exploitat da se skroz pinga nekoga fixat ako se stigne.
+            System.out.println("PONG primljen za " + responseTime + " ms");
+            Thread.sleep(5000);
         }
     }
 
@@ -175,11 +181,11 @@ public class Main_peer_to_peer {
         }
 
         if (message.getProtocolVersion() != NetworkMessage.CURRENT_PROTOCOL_VERSION) {
-            return "Protocol version nije podržan.";
+            return "Protocol version nije podrzan.";
         }
 
         if (!NETWORK_ID.equals(payload.getNetworkId())) {
-            return "Node pripada drugoj mreži.";
+            return "Node pripada drugoj mrezi.";
         }
 
         if (!GENESIS_HASH.equals(payload.getGenesisHash())) {
@@ -200,5 +206,118 @@ public class Main_peer_to_peer {
     private static boolean isKnownNodeType(String nodeType) {
 
         return "FULL".equals(nodeType) || "LIGHT".equals(nodeType) || "MINER".equals(nodeType);
+    }
+
+    private static void handleIncomingPeer(PeerConnection connection) {
+
+        try (connection) {
+            NetworkMessage receivedMessage = connection.receive();
+            if (receivedMessage == null) {
+                System.out.println("Peer se odspojio prije HELLO poruke.");
+
+                return;
+            }
+
+            if (receivedMessage.getType() != MessageType.HELLO) {
+
+                NetworkMessage rejectMessage = MessageCodec.createMessage(
+                        MessageType.REJECT,
+                        Ipconfig.NODE_ID,
+                        receivedMessage.getMessageId(),
+                        new RejectPayload(
+                                "EXPECTED_HELLO",
+                                "Prva poruka mora biti HELLO."));
+
+                connection.send(rejectMessage);
+                return;
+            }
+
+            HelloPayload peerInfo = MessageCodec.payloadAsPayloadTypeIWant(receivedMessage, HelloPayload.class);
+
+            String rejectionReason = validateHello(receivedMessage, peerInfo);
+
+            boolean accepted = rejectionReason == null;
+            HelloPayload ownInfo = null;
+
+            if (accepted) {
+                ownInfo = createHelloPayload();
+
+                System.out.println("Prihvacen node: " + receivedMessage.getSenderNodeId());
+
+                System.out.println("Node type: " + peerInfo.getNodeType());
+            } else {
+                System.out.println("HELLO odbijen: " + rejectionReason);
+            }
+
+            HelloAckPayload ackPayload = new HelloAckPayload(
+                    accepted,
+                    rejectionReason,
+                    ownInfo);
+
+            NetworkMessage ackMessage = MessageCodec.createMessage(
+                    MessageType.HELLO_ACK,
+                    Ipconfig.NODE_ID,
+                    receivedMessage.getMessageId(),
+                    ackPayload);
+
+            connection.send(ackMessage);
+
+            if (!accepted) { // oni koji su rejectani ovdje je njihov lifespan gotov
+                return;
+            }
+
+            // za one kod kojih je handshake gotov staviti da slušaju PING PONG
+            listenForMessages(connection, receivedMessage.getSenderNodeId());
+
+        } catch (Exception e) {
+            System.out.println("Greška tijekom komunikacije s peerom: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void listenForMessages(PeerConnection connection, String peerNodeId) throws Exception {
+        // ping pong
+
+        while (true) {
+            NetworkMessage message = connection.receive();
+            if (message == null) {
+                System.out.println("Peer se odspojio: " + peerNodeId);
+                return;
+            }
+
+            if (!peerNodeId.equals(message.getSenderNodeId())) {
+                System.out.println("Peer je promijenio node ID tijekom konekcije.");
+                return;
+            }
+
+            if (message.getType() == MessageType.PING) {
+                PingPayload pingPayload = MessageCodec.payloadAsPayloadTypeIWant(message, PingPayload.class);
+
+                PongPayload pongPayload = new PongPayload(pingPayload.getSentAt(), System.currentTimeMillis());
+
+                NetworkMessage pongMessage = MessageCodec.createMessage(
+                        MessageType.PONG,
+                        Ipconfig.NODE_ID,
+                        message.getMessageId(),
+                        pongPayload);
+
+                connection.send(pongMessage);
+
+                System.out.println("PING primljen od: " + peerNodeId);
+
+            } else {
+
+                NetworkMessage rejectMessage = MessageCodec.createMessage(
+                        MessageType.REJECT,
+                        Ipconfig.NODE_ID,
+                        message.getMessageId(),
+                        new RejectPayload(
+                                "MESSAGE_NOT_SUPPORTED",
+                                "Ova vrsta poruke još nije implementirana."));
+
+                connection.send(rejectMessage);
+            }
+        }
+
     }
 }
